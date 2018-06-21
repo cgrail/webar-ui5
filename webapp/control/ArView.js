a493bcaeae2fcbf3b8b56529c5577c37ef88148b
Christian Grail <christian.grail@sap.com>
2018-06-21 15:20:14 +0000

    WebXR version

--- webapp/control/ArView.js
+++ webapp/control/ArView.js
@@ -11,22 +11,11 @@ sap.ui.core.Control.extend("webar-test.control.ArView", {
 	},
 
 	getPosition: function() {
-		var vrFrameData = this.getThreeContext().vrFrameData;
-		return new THREE.Vector3(
-			vrFrameData.pose.position[0],
-			vrFrameData.pose.position[1],
-			vrFrameData.pose.position[2]
-		);
+		return this.getThreeContext().camera.position;
 	},
 
 	getOrientation: function() {
-		var vrFrameData = this.getThreeContext().vrFrameData;
-		return new THREE.Quaternion(
-			vrFrameData.pose.orientation[0],
-			vrFrameData.pose.orientation[1],
-			vrFrameData.pose.orientation[2],
-			vrFrameData.pose.orientation[3]
-		);
+		return this.getThreeContext().camera.quaternion;
 	},
 
 	onAfterRendering: function() {
@@ -35,117 +24,53 @@ sap.ui.core.Control.extend("webar-test.control.ArView", {
 			return;
 		}
 		this.arViewInitialized = true;
-
-		var vrDisplay;
-		var vrControls;
-		var arView;
-		var vrFrameData;
-
-		var canvas;
-		var camera;
-		var scene;
-		var renderer;
-
-		var viewId = this.getId();
+ 
 		var that = this;
+		var container;
+		var renderer, camera, scene;
+		THREE.WebXRUtils.getDisplays().then(init);
 
-		THREE.ARUtils.getARDisplay().then(function(display) {
-			if (display) {
-				vrFrameData = new VRFrameData();
-				vrDisplay = display;
-				init();
-			} else {
-				THREE.ARUtils.displayUnsupportedMessage();
-			}
-		});
-		
-		
-		/**
-		 * The render loop, called once per frame. Handles updating
-		 * our scene and rendering.
-		 */
-		function update() {
-			// Clears color from the frame before rendering the camera (arView) or scene.
-			renderer.clearColor();
+		function init(displays) {
+			container = document.createElement('div');
+			document.body.appendChild(container);
 
-			// Render the device's camera stream on screen first of all.
-			// It allows to get the right pose synchronized with the right frame.
-			arView.render();
-
-			// Update our camera projection matrix in the event that
-			// the near or far planes have updated
-			camera.updateProjectionMatrix();
-
-			// From the WebVR API, populate `vrFrameData` with
-			// updated information for the frame
-			vrDisplay.getFrameData(vrFrameData);
-
-			// Update our perspective camera's positioning
-			vrControls.update();
-
-			// Render our three.js virtual scene
-			renderer.clearDepth();
-			renderer.render(scene, camera);
-
-			TWEEN.update();
-
-			// Kick off the requestAnimationFrame to call this function
-			// when a new VRDisplay frame is rendered
-			vrDisplay.requestAnimationFrame(update);
-
-		}
-
-		function init() {
-			// Setup the three.js rendering environment
-			renderer = new THREE.WebGLRenderer({
-				alpha: true,
-			});
-			renderer.setPixelRatio(window.devicePixelRatio);
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			renderer.autoClear = false;
-			canvas = renderer.domElement;
-			document.body.appendChild(canvas);
 			scene = new THREE.Scene();
+			camera = new THREE.PerspectiveCamera();
+			scene.add(camera);
 			that.setThreeContext({
 				scene: scene,
-				vrFrameData: vrFrameData
+				camera: camera
 			});
-
-			// Creating the ARView, which is the object that handles
-			// the rendering of the camera stream behind the three.js
-			// scene
-			arView = new THREE.ARView(vrDisplay, renderer);
-
-			// The ARPerspectiveCamera is very similar to THREE.PerspectiveCamera,
-			// except when using an AR-capable browser, the camera uses
-			// the projection matrix provided from the device, so that the
-			// perspective camera's depth planes and field of view matches
-			// the physical camera on the device.
-			camera = new THREE.ARPerspectiveCamera(
-				vrDisplay,
-				60,
-				window.innerWidth / window.innerHeight,
-				vrDisplay.depthNear,
-				vrDisplay.depthFar
-			);
+			renderer = new THREE.WebGLRenderer({
+				alpha: true
+			});
+			renderer.autoClear = false;
+			container.appendChild(renderer.domElement);
+			
 			var pointLight = new THREE.PointLight(0xffffff, 0.8);
 			camera.add(pointLight);
 
 			var ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.4);
 			scene.add(ambientLight);
 
-			// VRControls is a utility from three.js that applies the device's
-			// orientation/position to the perspective camera, keeping our
-			// real world and virtual world in sync.
-			vrControls = new THREE.VRControls(camera);
+			window.addEventListener('resize', onWindowResize, false);
+			onWindowResize();
+			var options = {
+				// Flag to start AR if is the unique display available.
+				AR_AUTOSTART: true
+			}
+			renderer.xr = new THREE.WebXRManager(options, displays, renderer, camera, scene, update);
+		}
 
-			canvas.addEventListener(
-				'touchstart',
-				function() {
-				}, false);
-
-			// Kick off the render loop!
-			update();
+		function onWindowResize() {
+			setTimeout(() => {
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+			}, 150);
+		}
+		// Called once per frame, before render, to give the app a chance to update this.scene
+		function update(frame) {
+			TWEEN.update();
 		}
 
 	},
